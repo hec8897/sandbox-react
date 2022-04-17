50bd4c8beeefe7f0302f488d96669c1f901adfcc
dawoon <hec8897@naver.com>
2022-04-17 14:37:45 +0900

    tailwind-setting

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="columns-2">
+        <p>Well, let me tell you something, ...</p>
+        <p className="break-after-column">Sure, go ahead, laugh...</p>
+        <p>Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
